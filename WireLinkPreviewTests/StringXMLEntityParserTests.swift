2836a89b3cd37241e609cdbcde97e3dfa74e271c
bill chan <billypchan@gmail.com>
2020-10-05 16:40:41 +0200

    clean up

--- WireLinkPreviewTests/StringXMLEntityParserTests.swift
+++ WireLinkPreviewTests/StringXMLEntityParserTests.swift
@@ -19,7 +19,7 @@
 import XCTest
 @testable import WireLinkPreview
 
-class StringXMLEntityParserTests: XCTestCase {
+final class StringXMLEntityParserTests: XCTestCase {
     
     func testThatItIgnoresEmptyString() {
         // given 
